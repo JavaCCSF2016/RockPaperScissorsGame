157926f831548f209046848a90080adbeb0db570
LeoYulinLi <LeoYulinLi@gmail.com>
2016-04-17 10:55:59 -0700

    add betting

--- src/com/github/javaccsf2016/rockpaperscissorsgame/RPSGame.java
+++ src/com/github/javaccsf2016/rockpaperscissorsgame/RPSGame.java
@@ -11,15 +11,20 @@ public class RPSGame {
 	public static final int TIE = 1;
 	public static final int WIN = 2;
 	
+	public static final int INITIAL_MONEY = 1000;
+	
 	private int numberOfTies;
 	private int numberOfWins;
 	private int numberOfLoses;
 	private int move;
+	private int betMoney;
+	private int balance;
 	
 	public RPSGame() {
 		numberOfTies = 0;
 		numberOfWins = 0;
 		numberOfLoses = 0;
+		balance = INITIAL_MONEY;
 	}
 	
 	public void generateComputerPlay() {
@@ -27,16 +32,31 @@ public class RPSGame {
 		this.move = rand.nextInt(3);
 	}
 	
+	public boolean setBettingAmount(int money) {
+		if(money > 0 && money > this.balance) { 
+			this.betMoney = money;
+			this.balance -= money;
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
 	public int findWinner(int userMove) {
 		if (userMove >= ROCK && userMove <= SCISSOR) {
 			if (userMove == this.move) {
 				this.numberOfTies++;
+				this.balance += betMoney;
+				betMoney = 0;
 				return TIE;
 			} else if(userMove - this.move == 1 || userMove - this.move == -2) {
 				this.numberOfWins++;
+				this.balance += betMoney * 2;
+				betMoney = 0;
 				return WIN;
 			} else {
 				this.numberOfLoses++;
+				betMoney = 0;
 				return LOSE;
 			} 
 		} else {
