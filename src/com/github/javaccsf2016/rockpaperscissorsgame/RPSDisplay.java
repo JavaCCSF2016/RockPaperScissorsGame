b4b6868243a3a07bddc6e6641aa8195a6a041560
Leo Yulin Li <LeoYulinLi@gmail.com>
2016-04-20 15:57:07 -0700

    move commandLineGame to  a new class

--- src/com/github/javaccsf2016/rockpaperscissorsgame/RPSDisplay.java
+++ src/com/github/javaccsf2016/rockpaperscissorsgame/RPSDisplay.java
@@ -181,57 +181,9 @@ public class RPSDisplay extends JFrame {
 	public static void main(String args[]) {
 		// create an object of your class
 		RPSDisplay frame = new RPSDisplay();
-		commandLineGame();
+		RPSCommandLineGame.commandLineGame();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 	
-	public static void commandLineGame() {
-		RPSGame cliGame = new RPSGame();
-		displayCommandLineGameHeader();
-		boolean keepPlaying = true;
-		while (keepPlaying) {
-			displayCommandLineMenu();
-			keepPlaying = displayCommandLineGame(cliGame);
-		}
-	}
-	
-	public static void displayCommandLineGameHeader() {
-		System.out.println("RPS Game in command line");
-	}
-	
-	public static void displayCommandLineMenu() {
-		System.out.print(   "========================================\n" +
-							"0: Rock | 1: Paper | 2: Scissor  \n" +
-							"Type any other number to quit.\n"    +
-							"Your move: ");
-	}
-	
-	public static boolean displayCommandLineGame(RPSGame cliGame) {
-		Scanner scan = new Scanner(System.in);
-		int userInput = scan.nextInt();
-		cliGame.generateComputerPlay();
-		switch (cliGame.findWinner(userInput)) {
-		case 0: 
-			System.out.println("=== Computer won ===");
-			System.out.println("[STATS] Wins: " + cliGame.getWins() +
-							   " | Ties: " + cliGame.getTies() + 
-							   " | Loses: " + cliGame.getLoses());
-			return true;
-		case 1: 
-			System.out.println("=== Tie ===");
-			System.out.println("[STATS] Wins: " + cliGame.getWins() +
-					   " | Ties: " + cliGame.getTies() + 
-					   " | Loses: " + cliGame.getLoses());
-			return true;
-		case 2:
-			System.out.println("=== You won! ===");
-			System.out.println("[STATS] Wins: " + cliGame.getWins() +
-					   " | Ties: " + cliGame.getTies() + 
-					   " | Loses: " + cliGame.getLoses());
-			return true;
-		default:
-			System.out.println("Quit");
-			return false;
-		}
-	}
+
 }
